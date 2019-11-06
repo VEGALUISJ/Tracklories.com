e24f1249a03276794761c16bad6b45be166f9330
Jose Luis Vega <Luisvega.jlvv@gmail.com>
2019-11-06 21:19:40 +0000

    inicio de elaboracion food tracker view

--- src/js/views/food-tracker.js
+++ src/js/views/food-tracker.js
@@ -3,6 +3,49 @@ import React, { Component } from "react";
 export class Tracker extends Component {
 	state = {};
 	render() {
-		return <div className="container containerfoodtrack"> this is a test</div>;
+		return (
+			<div>
+				<div className="jumbotron jumbotron-fluid jumbotronfoodtracker mx-auto">
+					<div className="container">
+						<h1>Start tracking how much calories you eat is the first step to a healthy life!!!</h1>
+						<p className="lead">
+							Here you can track how many calorie you ete in previus days, and take control about your
+							nutrition.
+						</p>
+					</div>
+				</div>
+				<div className="container">
+					<div className="col">
+						<div className="container1 col-2">test 1</div>
+						<div className="container2 col-8">test 2</div>
+						<div className="container3 col-2">test 3</div>
+					</div>
+					<div className="container">
+						<div className="row">
+							<div className="col">
+								<p>insertar barra de progreso</p>
+							</div>
+							<div className="col">
+								<div>
+									<p>Insertar fecha</p>
+								</div>
+								<div>
+									<p>Insertar fecha</p>
+								</div>
+								<div>
+									<p>Insertar fecha</p>
+								</div>
+								<div>
+									<p>Insertar fecha</p>
+								</div>
+								<div>
+									<p>Insertar fecha</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
