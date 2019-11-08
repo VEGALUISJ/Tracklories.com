b1c9f1e049dc8fc7416def9f6edcc172c7b236f6
Jose Luis Vega <Luisvega.jlvv@gmail.com>
2019-11-08 23:58:46 +0000

    fixing footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,35 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="page-footer font-small special-dark bg-dark">
-		<div className="container">
-			<div className="row">
-				<div className="col-md-6 mb-4" />
-				<form className="form-inline" />
-				<input
-					className="form-control form-control-sm mr-3 w-75"
-					type="text"
-					placeholder="Search"
-					aria-label="Search"
-				/>
-
-				<i className="fas fa-search" aria-hidden="true" />
-				<div className="col-md-6 mb-4" />
-				<form className="input-group" />
-				<input
-					className="form-control form-control-sm"
-					type="text"
-					placeholder="Your email"
-					aria-label="Your email"
-					aria-describedby="basic-addon2"
-				/>
-				<div className="input-group-append">
-					<button className="btn btn-sm btn-outline-white my-0" type="button">
-						Sign up
-					</button>
-				</div>
-			</div>
-		</div>
+	<footer className="page-footer footer1 font-small special-dark bg-dark">
 		<p className="mx-auto text-white text-center">
 			Find us in <br />
 			<i className="fab fa-facebook mr-2" />
@@ -42,8 +14,8 @@ export const Footer = () => (
 			</a>
 		</p>
 		<div className="footer-copyright text-center py-3">
-			© 2019 Copyright:
-			<a href="https://mdbootstrap.com/education/bootstrap/">tracklories.com</a>
+			Copyright © 2019:
+			<a href="https://mdbootstrap.com/education/bootstrap/"> tracklories.com</a>
 		</div>
 	</footer>
 );
